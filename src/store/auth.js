2bc2028419199bf08e1a206c6c4bb7a57e41bd6c
sam <chtlstjd01@gmail.com>
2020-01-21 20:58:32 +0900

    로그인 reducer 작성

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,16 +1,64 @@
-const LOGIN = 'auth/LOGIN';
+import * as api from '../api';
+
+const LOGIN_REQUEST = 'auth/LOGIN_REQUEST';
+const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 const LOGOUT = 'auth/LOGOUT';
 
-export default function auth(state = [], action) {
-  switch (action.type) {
-    case LOGIN:
-      return {
+export const loginRequest = () => ({
+  type: LOGIN_REQUEST,
+});
 
-      };
-    case LOGOUT:
-      return {
+export const loginSuccess = (userInfo) => ({
+  type: LOGIN_SUCCESS,
+  payload: {
+    userInfo,
+  },
+});
 
+export const loginFailure = () => ({
+  type: LOGIN_FAILURE,
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
+
+export const loginThunk = (email, password) => (dispatch) => {
+  dispatch(loginRequest());
+
+  return api.login(email, password).then(
+    (result) => {
+      if (result.data) {
+        dispatch(loginSuccess(result.data));
+        return true;
       }
+      dispatch(loginFailure());
+      return false;
+    },
+  ).catch((error) => {
+    console.log(error);
+    dispatch(loginFailure());
+  });
+};
+
+const initialState = {
+  isAuthenticated: false,
+  userInfo: null,
+};
+
+export default function auth(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        isAuthenticated: true,
+        userInfo: action.payload,
+      };
+    case LOGIN_FAILURE: case LOGOUT:
+      return {
+        isAuthenticated: false,
+        userInfo: null,
+      };
     default:
       return state;
   }
